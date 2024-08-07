84456300f64e48d25cc2dc85c29fad0eade1c419
CW-Johirull <mdjohirull.codeware@gmail.com>
2024-08-07 18:03:09 +0600

    Routing class add

---
+++ hrms/Routs/Routing.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace SigmaERP.hrms.Routs
+{
+    public static class Routing
+    {
+        public static string UserPermissionName = "PermissionsRoute";
+        public static string IndexRouteUrl = "permissions";
+        public static string IndexRoutePhysicalFile = "../userPermissions.aspx";
+
+        // Method to register routes
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            // Register the route
+            routes.MapPageRoute("PermissionsRoute", "permissions", "IndexRoutePhysicalFile");
+        }
+    }
+
+   
+}
