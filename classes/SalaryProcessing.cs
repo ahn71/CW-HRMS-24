a14b9f024dd8054108d38633cda4af7e79cace86
RakibCodeware <rakibul.codeware@gmail.com>
2025-12-01 18:45:07 +0600

    solve absnet deduction pblm

--- classes/SalaryProcessing.cs
+++ classes/SalaryProcessing.cs
@@ -1119,7 +1119,7 @@ namespace SigmaERP.classes
                     if (rule["maxAbsent"] != null)
                     {
                         int maxAbsent = (int)rule["maxAbsent"];
-                        if (absentDays < maxAbsent)
+                        if (absentDays <= maxAbsent)
                         {
                             if (rule["deductFrom"].ToString() == "Basic")
                             {
